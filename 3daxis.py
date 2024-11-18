e1346ba7b903196c93c26a2bab5eb59d04c224a7
Devdatta Talele <50290838+devdattatalele@users.noreply.github.com>
2024-11-18 18:15:52 +0530

    Update 3daxis.py

--- 3daxis.py
+++ 3daxis.py
@@ -1,1 +1,1 @@
-"%PROGRAMFILES(X86)%\Google\Chrome Remote Desktop\CurrentVersion\remoting_start_host.exe" --code="4/0AeanS0YxETYVBgC1-unDW1PpsWT4bUXuaKlMKG2S1nfDQ-ITSclfhlFz9EXUdD6IYMhzuA" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=%COMPUTERNAME%
+DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="4/0AeanS0YxETYVBgC1-unDW1PpsWT4bUXuaKlMKG2S1nfDQ-ITSclfhlFz9EXUdD6IYMhzuA" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$(hostname)
